deff1dac382b67efc953e133ed173d25c1ddccd5
Abhinav Agarwal <60694856+Abhinavzbio@users.noreply.github.com>
2021-11-10 17:21:37 +0530

    removing console logs

--- src/Pages/showDetails/showDetails.page.jsx
+++ src/Pages/showDetails/showDetails.page.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import ImageView from "../../Components/imageView/imageView.component";
-// import "./showDetails.style.css";
+
 
 class ShowDetails extends Component {
 
@@ -11,18 +11,14 @@ class ShowDetails extends Component {
     const selectedImage = images.filter((item) =>
       match.url.includes(item.data.id)
     );
-    console.log("showDetails props", this.props);
-    console.log("Filtered", match);
+    
     return (
       <div>
         <ImageView image={selectedImage} />
       </div>
     );
   }
-  componentWillUnmount() {
-      const {history, match} = this.props
-      history.push(`${match.url}`);
-  }
+ 
 }
 
 export default withRouter(ShowDetails);
