a12445fed2b86004e264a760dd189b99cdd53ede
Abhinav Agarwal <60694856+Abhinavzbio@users.noreply.github.com>
2021-11-10 17:20:50 +0530

    removing console logs and comments

--- src/Pages/homePage/homePage.page.jsx
+++ src/Pages/homePage/homePage.page.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-// import { Switch, Route, Redirect } from 'react-router-dom';
 import { CardList } from "../../Components/card-list/card-list.component";
 import { Searchbox } from "../../Components/searchBox/searchBox.component";
 import  "./homePage.style.css";
@@ -10,7 +9,7 @@ class HomePage extends Component {
     this.state = {
       searchInput: "",
     };
-    // this.handleChange = this.handleChange.bind(this);
+    
   }
 
   
@@ -24,11 +23,11 @@ class HomePage extends Component {
   render() {
     const {images} = this.props;
     const {  searchInput } = this.state;
-    // console.log(data);
+ 
     const filteredImages = images.filter((item) =>
       item.data.title.toLowerCase().includes(searchInput.toLowerCase())
     );
-    console.log("Filtered",filteredImages)
+    
     return (
       <div >
         <h1 className="heading">Gallery</h1>
